b26ed4dc83caceacdbb8038ea51a6bb18cbde90b
DefNotArham <arhamkabir231@gmail.com>
2026-09-12 22:13:32 -0400

    Update number-guessing-game.py

--- number-guessing-game.py
+++ number-guessing-game.py
@@ -4,13 +4,14 @@ num = random.randint(1, 100)
 
 while True:
     guess = int(input("Guess the number: "))
+    guessCounter = 0
 
     if guess == num:
         print("You guessed it!")
         break
     elif guess > num:
         print("Lower")
+        guessCounter = guessCounter + 1
     elif guess < num:
         print("Higher")
-    else:
-        print("Wrong!")
+        guessCounter = guessCounter + 1
